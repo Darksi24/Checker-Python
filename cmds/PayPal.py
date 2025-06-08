07f62a5a0b168991af4857d8596a0e2306622a85
Darksi24 <yr84865@gmail.com>
2025-06-08 12:07:02 -0600

    Update PayPal.py

--- cmds/PayPal.py
+++ cmds/PayPal.py
@@ -19,8 +19,46 @@ async def st_handler(msg: types.Message):
 
     # Ahora tu request
     try:
-        cookies = { ... }  # aquí pegas tus cookies completas
-        headers = { ... }  # y tus headers también
+        cookies = {
+            'enforce_policy': 'ccpa',
+            'cookie_check': 'yes',
+            'd_id': 'ab077583653a4cf4b23c51c99dd9255e1747451999479',
+            'datadome': 'iYAkUQaQ8z7H0lYZZGu~bt9W8zXuS0Ic5zWZZMRMmRAOt5rjm5l1xpDxZZMDs_pWS7zZJ5w61f9xZ0_0~ywCVw9Bp9g3kUbdM1lYeDaYIIrAA~QxcBFcJz9a6~lEsQFG',
+            'nsid': 's%3ADeYReZf8QMjXskwrP24hYROvFhJChZgL.ZKHe6X40dqNDBjl40N0lQrEo3H%2BpvlAUL1nxQAsgdoM',
+            'ts_c': 'vr%3D70ae65981940aa5a1023f2a2fe826b6c%26vt%3D509c5f841970a798285cadbbfd2f373e',
+            'cookie_prefs': 'T%3D0%2CP%3D1%2CF%3D1%2Ctype%3Dimplicit',
+            'l7_az': 'dcg16.slc',
+            'LANG': 'en_US%3BUS',
+            'rssk': 'd%7DC9%40%3A%3C9592%3A%3E%3B97%3Exqx%3E9wp9pm%7Dq%3F16',
+            'KHcl0EuY7AKSMgfvHl7J5E7hPtK': 'hKLin-hfAWz8cAy4Ain9wxOKflNSrOOlwgMQd89LxKkoWSg6Qven-x_ssl7CjlN-2Ejc2UQk16hBXhlr',
+            'ddi': 'EbfWe9YmOuLCOp1C3h3sQl-hMS0RfQcXUGkrs7kcBsfZ8Yu7DnV7v4q4GB8IvgBAA2zcAM6F7wycT3rhLvsuIGB15WD7y7YGUg5D2hdxzj00ysqg',
+            'sc_f': 'qihcsYKN-aVZn2i5BF9UEJPof-RywU9De_rJHTFhe60AOGAgazR-8XhxFyh-kox9eRxBCx1RCfxXDwtbhSJ0VTv6k7t_JwFyXz5rpW',
+            'ts': 'vreXpYrS%3D1780941295%26vteXpYrS%3D1749407095%26vr%3D70ae65981940aa5a1023f2a2fe826b6c%26vt%3D509c5f841970a798285cadbbfd2f373e%26vtyp%3Dreturn',
+            'tsrce': 'standardcardfields',
+            'x-pp-s': 'eyJ0IjoiMTc0OTQwNTI5NTgwNCIsImwiOiIwIiwibSI6IjAifQ',
+            
+        }  # aquí pegas tus cookies completas
+        headers = {
+            'authority': 'www.paypal.com',
+            'accept': '*/*',
+            'accept-language': 'es-CL,es-419;q=0.9,es;q=0.8',
+            'content-type': 'application/json',
+            # 'cookie': 'enforce_policy=ccpa; cookie_check=yes; d_id=ab077583653a4cf4b23c51c99dd9255e1747451999479; datadome=iYAkUQaQ8z7H0lYZZGu~bt9W8zXuS0Ic5zWZZMRMmRAOt5rjm5l1xpDxZZMDs_pWS7zZJ5w61f9xZ0_0~ywCVw9Bp9g3kUbdM1lYeDaYIIrAA~QxcBFcJz9a6~lEsQFG; nsid=s%3ADeYReZf8QMjXskwrP24hYROvFhJChZgL.ZKHe6X40dqNDBjl40N0lQrEo3H%2BpvlAUL1nxQAsgdoM; ts_c=vr%3D70ae65981940aa5a1023f2a2fe826b6c%26vt%3D509c5f841970a798285cadbbfd2f373e; cookie_prefs=T%3D0%2CP%3D1%2CF%3D1%2Ctype%3Dimplicit; l7_az=dcg16.slc; LANG=en_US%3BUS; rssk=d%7DC9%40%3A%3C9592%3A%3E%3B97%3Exqx%3E9wp9pm%7Dq%3F16; KHcl0EuY7AKSMgfvHl7J5E7hPtK=hKLin-hfAWz8cAy4Ain9wxOKflNSrOOlwgMQd89LxKkoWSg6Qven-x_ssl7CjlN-2Ejc2UQk16hBXhlr; ddi=EbfWe9YmOuLCOp1C3h3sQl-hMS0RfQcXUGkrs7kcBsfZ8Yu7DnV7v4q4GB8IvgBAA2zcAM6F7wycT3rhLvsuIGB15WD7y7YGUg5D2hdxzj00ysqg; sc_f=qihcsYKN-aVZn2i5BF9UEJPof-RywU9De_rJHTFhe60AOGAgazR-8XhxFyh-kox9eRxBCx1RCfxXDwtbhSJ0VTv6k7t_JwFyXz5rpW; ts=vreXpYrS%3D1780941295%26vteXpYrS%3D1749407095%26vr%3D70ae65981940aa5a1023f2a2fe826b6c%26vt%3D509c5f841970a798285cadbbfd2f373e%26vtyp%3Dreturn; tsrce=standardcardfields; x-pp-s=eyJ0IjoiMTc0OTQwNTI5NTgwNCIsImwiOiIwIiwibSI6IjAifQ',
+            'origin': 'https://www.paypal.com',
+            'paypal-client-context': '8UA0911501956764B',
+            'paypal-client-metadata-id': '8UA0911501956764B',
+            'referer': 'https://www.paypal.com/smart/card-fields?sessionID=uid_c4deb6989a_mtc6nte6mtk&buttonSessionID=uid_90427d3405_mtc6ntm6mzi&locale.x=en_US&commit=true&hasShippingCallback=false&env=production&country.x=US&sdkMeta=eyJ1cmwiOiJodHRwczovL3d3dy5wYXlwYWwuY29tL3Nkay9qcz9lbmFibGUtZnVuZGluZz12ZW5tbyZjbGllbnQtaWQ9QVVJbGtrdzY1bUg0b2lmcDZmWGtYdXNQMXdBNVJybTNsLXNWbnpxRVlacENWUTY0dHdIRENGRlFCRnZCSExwQ09GRHEza1hRNnhlTHd3djQmY3VycmVuY3k9RVVSJmxvY2FsZT1lbl9VUyZkaXNhYmxlLWZ1bmRpbmc9Z2lyb3BheSUyQ3NlcGElMkNzb2ZvcnQiLCJhdHRycyI6eyJkYXRhLXVpZCI6InVpZF9scGh0ZGRreXBsbWlndWlpc29icnBwbHRheXBsbnoifX0&disable-card=&token=8UA0911501956764B',
+            'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
+            'x-app-name': 'standardcardfields',
+            'x-country': 'US',
+
+        }  # y tus headers también
 
         json_data = {
             'query': '...',
