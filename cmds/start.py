b6d8e18d71a0798dbcf5490379ed2eb9371ef87d
Darksi24 <yr84865@gmail.com>
2025-06-16 04:02:02 +0000

    Nuevos Ajutes

--- cmds/start.py
+++ cmds/start.py
@@ -13,21 +13,25 @@ async def cmds_handler(msg: types.Message):
             InlineKeyboardButton(text="𝑻𝑶𝑶𝑳𝑺", callback_data="tools")
         ]
     ])
-    photo_url = "https://imgur.com/a/8Zxhoug"
-    await msg.answer_photo(photo_url, caption="📜 𝐵𝐼𝐸𝑁𝑉𝐸𝑁𝐼𝐷𝑂 𝐴𝐿 𝑀𝑂𝐷𝑂 𝐷𝐸 𝑃𝑅𝑈𝐸𝐵𝐴", reply_markup=keyboard)
+    photo_url = "https://imgur.com/a/8Zxhoug.jpg"
+    await msg.answer_photo(photo_url, caption="📜 WELCOME!!", reply_markup=keyboard)
 
 # Callback que edita el mensaje original
 @router.callback_query(F.data == "show_cmds")
 async def show_cmds_callback(callback: types.CallbackQuery):
     await callback.message.edit_caption(
-        "★-★-★-★-★-★-★-★-★-★-★\n\n"
-        "⟨⟨ Stripe Auth ⟩⟩\n"
-        "⟨⟨ Status: On ✅ ⟩⟩\n"
-        "⟨⟨ Use: /auth CC|M|Y|CVV ⟩⟩\n"
+        caption=(
         "★-★-★-★-★-★-★-★-★-★-★\n"
-        "⟨⟨ Square ⟩⟩\n"
-        "⟨⟨ Status: off 🚫⟩⟩\n"
-        "⟨⟨ Use: /sq CC|M|Y|CVV ⟩⟩\n",
+        "⟨⟨ *Stripe Auth* ⟩⟩\n"
+        "⟨⟨ *Status:* On ✅ ⟩⟩\n"
+        "⟨⟨ *Use:* /auth CC|M|Y|CVV ⟩⟩\n"
+        "★-★-★-★-★-★-★-★-★-★-★\n"
+        "⟨⟨ *Square* ⟩⟩\n"
+        "⟨⟨ *Status:* off 🚫⟩⟩\n"
+        "⟨⟨ *Use:* /sq CC|M|Y|CVV ⟩⟩\n"
+        "★-★-★-★-★-★-★-★-★-★-★\n"
+        ),
+        parse_mode="Markdown",
         reply_markup=None  # Quita los botones después
     )
     await callback.answer()  # Para evitar el "loading..." eterno
